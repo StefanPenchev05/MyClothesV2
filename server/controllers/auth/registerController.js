5539b8421d36b547ac420469883a659823014397
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-03-13 09:54:27 +0200

    Add Settigns Model with Fields of General, Privacy and  Security #50

--- server/controllers/auth/registerController.js
+++ server/controllers/auth/registerController.js
@@ -9,6 +9,8 @@ import { User } from "../../models/User.js";
 import { Validator } from "../../utils/validator.js";
 import { Temp } from "../../models/Temp.js";
 
+// This function generates a unique username by appending a random number to the provided username
+// It checks both the User and Temp collections to ensure the username is unique
 async function generateUniqueUsername(username) {
   let newUsername = username;
   let userExists = true;
@@ -23,10 +25,13 @@ async function generateUniqueUsername(username) {
 }
 
 export default async function registerController(req, res) {
+  // Extract user details from the request body
   const { firstName, lastName, email, username, password } = req.body;
 
+  // Array to store any validation errors
   const inputErrors = [];
 
+  // Validate each input field and add any error messages to the inputErrors array
   const isFirstAndLastNameValid = Validator.isFirstAndLastName(firstName, lastName);
   if (typeof isFirstAndLastNameValid === "string") {
     inputErrors.push(isFirstAndLastNameValid);
@@ -47,6 +52,7 @@ export default async function registerController(req, res) {
     inputErrors.push(isPasswordValid);
   }
 
+  // Check if password and username are the same or if password contains the username
   if (password === username) {
     inputErrors.push("The password and username cannot be the same");
   }
@@ -55,19 +61,22 @@ export default async function registerController(req, res) {
     inputErrors.push("Password should not contain the username");
   }
 
+  // If there are any validation errors, return a 400 status code and the error messages
   if (inputErrors.length > 0) {
     return res.status(400).json({ messages: inputErrors });
   }
 
   try {
-    //get the ip of the user
+    // Get the user's IP address
     const ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
            req.connection.remoteAddress || 
            req.socket.remoteAddress || 
            req.connection.socket.remoteAddress;
            
+    // Hash the user's password
     const hashPassword = await bcrypt.hash(password, 12);
 
+    // Check if the username is already in use in the Temp collection
     const tempUsername = await Temp.exists({'value.username': username});
 
     if(tempUsername){
@@ -80,12 +89,12 @@ export default async function registerController(req, res) {
       throw error;
     }
 
-    //generate a UUID
+    // Generate a UUID and a JWT for email verification
     const uuid = uuidv4();
-    
     const verificationToken = jwt.sign({uuid}, process.env.JWT_SECRET, { expiresIn: '15m' });
 
-    // Save the user data temporarily in Redis with a 15 minute expiry
+    // Temporarily store the user's data in Redis with a 15 minute expiry
+    // Then send a verification email to the user
     await setTempMemory(uuid, {
         ip,
         firstName,
@@ -94,15 +103,16 @@ export default async function registerController(req, res) {
         username,
         password: hashPassword
     }, 60 * 15).then(() => {
-        // Send the email with the link to verification
         sendVerifyMail(email, verificationToken, username);
     }).catch(err => {
         throw err;
     });
 
+    // If everything is successful, return a 200 status code and a success message
     return res.status(200).json({message: "Waiting for you to verify with link sended to your email"})
 
   } catch (err) {
+    // If the username is already taken, suggest a new one by calling the generateUniqueUsername function
     if (err.errors.username) {
       const newUsername = await generateUniqueUsername(username);
       return res
@@ -112,6 +122,7 @@ export default async function registerController(req, res) {
           newUsername,
         });
     }
+    // If there's any other error, return a 401 status code and the error message
     return res.status(401).json({ message: err });
   }
 }
