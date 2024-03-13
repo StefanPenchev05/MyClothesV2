d4456d528c8f7e9d57a25c0f98a6600de281f59a
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-03-13 11:40:16 +0200

    Fix token expiration issue and update email verification link

--- server/controllers/auth/verifyUser.js
+++ server/controllers/auth/verifyUser.js
@@ -19,15 +19,25 @@ export default async function verifyUser(req, res) {
     try {
       // Verify the token using the secret key
       decoded = jwt.verify(verificationToken, process.env.JWT_SECRET);
-    } catch (err) {
+    } catch (err){
+      console.log(err)
       // If the token is invalid, log the error and return an error response
       return res.status(401).json({ message: "Invalid token" });
     }
 
-    // Find the temporary user data associated with the token
-    const tempUserData = (await Temp.findOne({ key: decoded.uuid }, "value"))[
-      "value"
-    ];
+    console.log(decoded);
+
+    let tempUserData;
+    try{
+      // Find the temporary user data associated with the token
+      tempUserData = (await Temp.findOne({ key: decoded.uuid }, "value"))[
+        "value"
+      ];
+    }catch(err){
+      return res
+          .status(401)
+          .json({ message: "Invalid token, your time has expired" });
+    }
 
     // If no temporary user data was found, the token is invalid or expired
     if (!tempUserData) {
@@ -78,6 +88,7 @@ export default async function verifyUser(req, res) {
     // The event data includes a 'verified' property set to true
     io.to(decoded.uuid).emit('verificationComplete', { verified: true });
   } catch (err) {
+    console.log(err)
     // If an error occurred, log the error and send an error response
     return res.status(500).json({ message: "An error occurred" });
   }
