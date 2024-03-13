0c8615fd7be24fb1e1f6491d6c80ceea91188477
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-03-13 12:32:38 +0200

    Add Middleware for parseUserAgent so i can use it everywhere #50

--- server/controllers/auth/registerController.js
+++ server/controllers/auth/registerController.js
@@ -28,7 +28,6 @@ async function generateUniqueUsername(username) {
 export default async function registerController(req, res) {
   // Extract user details from the request body
   const { firstName, lastName, email, username, password } = req.body;
-  console.log(username)
 
   // Array to store any validation errors
   const inputErrors = [];
@@ -63,39 +62,15 @@ export default async function registerController(req, res) {
     inputErrors.push("Password should not contain the username");
   }
 
-  console.log(useragent.parse(req.headers['user-agent']));
-
   // If there are any validation errors, return a 400 status code and the error messages
   if (inputErrors.length > 0) {
     return res.status(400).json({ messages: inputErrors });
   }
 
   try {
-    // Get the user's IP address
-    const ip = (req.headers['x-forwarded-for'] || '').split(',').pop().trim() || 
-           req.connection.remoteAddress || 
-           req.socket.remoteAddress || 
-           req.connection.socket.remoteAddress;
-    
-    //Parse the User-Agent string
-    const agent = useragent.parse(req.headers['user-agent']);
-    const userAgent = agent.toString(); // Full user-agent string
-    const browser = agent.toAgent(); // Browser name and version
-    const operatingSystem = agent.os.toString(); // Operating system name and version
-    let deviceType = agent.os.family; // Get the operating system from the parsed user agent
 
-    // Check if the operating system matches any of the common mobile operating systems
-    if(deviceType.match(/Android|iOS|Windows Phone/i)){
-      // If it does, set deviceType to "isMobile"
-      deviceType = "isMobile";
-    }else if(deviceType.match(/Windows NT|Mac OS X|Linux/i)){
-      // If it doesn't match mobile but matches common desktop operating systems, set deviceType to "isDesktop"
-      deviceType = "isDesktop";
-    }else{
-      // If it doesn't match either mobile or desktop, set deviceType to "undefined"
-      deviceType = "undefined";
-    }
-           
+    const { ip, userAgent, browser, operatingSystem, deviceType } = req.userAgent;
+    
     // Hash the user's password
     const hashPassword = await bcrypt.hash(password, 12);
 
